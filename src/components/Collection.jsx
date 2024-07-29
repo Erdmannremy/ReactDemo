5f34603bed14f8b3143e37de91ae8d86e5721e06
erdmannremy@gmail.com <erdmannremy@gmail.com>
2024-07-29 17:12:33 +0200

    Msj-3

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -7,22 +7,35 @@ const Collection = () => {
 return (
     <div className="collection">
     <Photo 
-    img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true" 
+    img="https://st2.depositphotos.com/thumbs/1003723/image/5625/56251673/api_thumb_450.jpg?forcejpeg=true" 
     title="voyage"
     caption="Mon Départ"
     />
     <Photo 
     img="https://st.depositphotos.com/thumbs/2890953/image/4275/42751141/api_thumb_450.jpg?forcejpeg=true"
     title="Voyage"
-    caption="En route pour l'aventure"   
+    caption="En route vers l'aventure destination..."   
     />
-
     <Photo 
     img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true"
     title="voyage"
-    caption="Au Bout Du Monde"   
+    caption="Arrivé à destination Au Bout Du Monde"   
     />
-        
+      <Photo 
+    img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true"
+    title="voyage"
+    caption="Arrivé à destination Au Bout Du Monde"   
+    />
+    <Photo 
+    img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true"
+    title="voyage"
+    caption="Arrivé à destination Au Bout Du Monde"   
+    />
+    <Photo 
+    img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true"
+    title="voyage"
+    caption="Arrivé à destination Au Bout Du Monde"   
+    />  
         
         </div>
 );
