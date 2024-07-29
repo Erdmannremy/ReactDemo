d8864969098bf446492b53f993585a94bec85a99
erdmannremy@gmail.com <erdmannremy@gmail.com>
2024-07-29 16:48:17 +0200

    Demo-2-29/07/24

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -16,6 +16,12 @@ return (
     title="voyage"
     caption="Au Bout Du Monde"   
     />
+
+    <Photo 
+    img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true"
+    title="voyage"
+    caption="Au Bout Du Monde"   
+    />
         
         
         </div>
