7284c4607dd55c18396030571374829fa21b1c56
erdmannremy@gmail.com <erdmannremy@gmail.com>
2024-07-29 16:59:34 +0200

    Msj-3/29/07/24

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -9,12 +9,12 @@ return (
     <Photo 
     img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true" 
     title="voyage"
-    caption="Au Bout Du Monde"
+    caption="Mon Départ"
     />
     <Photo 
-    img="https://st.depositphotos.com/thumbs/1102719/image/4453/44532821/api_thumb_450.jpg?forcejpeg=true"
-    title="voyage"
-    caption="Au Bout Du Monde"   
+    img="https://st.depositphotos.com/thumbs/2890953/image/4275/42751141/api_thumb_450.jpg?forcejpeg=true"
+    title="Voyage"
+    caption="En route pour l'aventure"   
     />
 
     <Photo 
