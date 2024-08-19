ee318f463d375b1b6eb9dbdbc35d96566a6a2b81
erdmannremy@gmail.com <erdmannremy@gmail.com>
2024-08-19 10:34:11 +0200

    FooterMSJ190824-2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import React from "react";
 import Collection from "./components/Collection";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
     <div>
       <Header/>
       <Collection/>
+      <Footer/>
       
       {/* <Router>
         <nav>
